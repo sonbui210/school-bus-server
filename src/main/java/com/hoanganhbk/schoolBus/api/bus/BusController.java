129a3367e77fd7d1c5351d6b41eed92607630926
Sơn Bùi <79527137+sonbui210@users.noreply.github.com>
2021-06-20 22:10:58 +0700

    update
    api key gg map 5

--- src/main/java/com/hoanganhbk/schoolBus/api/bus/BusController.java
+++ src/main/java/com/hoanganhbk/schoolBus/api/bus/BusController.java
@@ -38,7 +38,7 @@ public class BusController {
 
         HttpEntity httpEntity = new HttpEntity(headers, null);
 
-        URL = URL + query + "&key=AIzaSyAlgnZw09Fkc0ndaLAkDyBBcFTzM-BSGcc";
+        URL = URL + query + "&key=AIzaSyDaOulQACiJzBfqumbsqg_-vKha8fCnL-s";
 
         ResponseEntity<String> response = restTemplate.exchange(URL,  HttpMethod.GET, httpEntity, String.class);
 

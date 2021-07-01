e9f5fdceb59e65c92b720dc9cd9ef75836b4288e
Sơn Bùi <79527137+sonbui210@users.noreply.github.com>
2021-07-01 17:14:19 +0700

    mail
    tat mai tam thoi

--- src/main/java/com/hoanganhbk/schoolBus/api/student/StudentController.java
+++ src/main/java/com/hoanganhbk/schoolBus/api/student/StudentController.java
@@ -100,7 +100,7 @@ public class StudentController {
             // Send Message!
             this.emailSender.send(message);
 
-            /*
+            
             //send email to hoc sinh
             SimpleMailMessage message1 = new SimpleMailMessage();
             message1.setTo(student.getEmailStudent());
@@ -108,7 +108,7 @@ public class StudentController {
             message1.setText("Ban vua " + action + " " + (bus != null ? bus.getCode() : "unknown") + " lúc " + now);
             // Send Message!
             this.emailSender.send(message1);
-            */
+            
         }
 
         */

a965a38b7d86122d4a2d26faab138b1ca9cbea47
sonbui <sonbui0298@gmail.com>
2021-07-13 17:59:48 +0700

    test bus id in student

--- src/main/java/com/hoanganhbk/schoolBus/data/model/Student.java
+++ src/main/java/com/hoanganhbk/schoolBus/data/model/Student.java
@@ -33,10 +33,10 @@ public class Student extends AuditModel{
 
     private String emailGrand;
 
-
+/*
     @ManyToOne
-    @JoinColumn(name = "bus_id")
-    private Bus bus;
+    @JoinColumn(name = "bus_id") */
+    private String bus;
 
     @Override
     public boolean equals(Object o) {

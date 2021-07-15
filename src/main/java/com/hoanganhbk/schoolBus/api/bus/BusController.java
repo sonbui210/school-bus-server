a4ffbf3fbd330b97d326845b016a33e56b405373
sonbui <sonbui0298@gmail.com>
2021-07-15 18:25:38 +0700

    upload key api

--- src/main/java/com/hoanganhbk/schoolBus/api/bus/BusController.java
+++ src/main/java/com/hoanganhbk/schoolBus/api/bus/BusController.java
@@ -38,7 +38,7 @@ public class BusController {
 
         HttpEntity httpEntity = new HttpEntity(headers, null);
 
-        URL = URL + query + "&key=AIzaSyDJVB0hTqJMdb1rIilxAAMEbcOcWioisg8";
+        URL = URL + query + "&key=AIzaSyBvrWzYjC5SmROtfLWwprBmuj2RiQwTqDI";
 
         ResponseEntity<String> response = restTemplate.exchange(URL,  HttpMethod.GET, httpEntity, String.class);
 
